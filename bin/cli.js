450dcb50cb041695fa6556b6633adb0b37ff82c9
Daniel Mahon <daniel@mahonstudios.com>
2013-02-06 12:42:32 -0500

    added support for aws creds and other options through cli updated pkgs

--- bin/cli.js
+++ bin/cli.js
@@ -1,26 +1,30 @@
 #!/usr/bin/env node
 
-var getProps = require('../lib/getProps.js')
-  , push = require('../lib/push.js')
-  , statIsGit = require('../lib/statIsGit.js')
-  , cp = require('../lib/cp.js')
-  , cpr = require('../lib/cpr.js')
-  , statSync = require('fs').statSync
+// var getProps = require('../lib/getProps.js')();
+var program = require('commander');
+var push = require('../lib/push.js');
+var statIsGit = require('../lib/statIsGit.js');
+var cp = require('../lib/cp.js');
+var cpr = require('../lib/cpr.js');
+var statSync = require('fs').statSync;
+var pjson = require('../package.json');
 
-;(function () {
-  var arg = process.argv.splice(2)
-    , props = getProps()
-    , path = arg[0] || process.cwd();
+program.version(pjson.version).option('-p, --path [value]', 'Set an path for upload').option('-k, --key [value]', 'AWS Key').option('-s, --secret [value]', 'AWS Secret Key').option('-b, --bucket [value]', 'AWS S3 Bucket').parse(process.argv);
 
-    if (args[1]) {
-    	prop.endpoint = args[1];
-    }
+(function() {
+	var options = {};
+	var props = require('../lib/getProps.js')(program);
+	var path = program.args[0] || process.cwd();
 
-  if (statIsGit(path)) {
-    push(props, path);
-  } else if (statSync(path).isFile()) {
-    cp(props, arg);
-  } else {
-    cpr(props, path);
-  }
+	if (program.path) {
+		options.path = program.path;
+	}
+
+	if (statIsGit(path)) {
+		push(props, path);
+	} else if (statSync(path).isFile()) {
+		cp(props, program.args);
+	} else {
+		cpr(props, path, options);
+	}
 })();
--- index.js
+++ index.js
@@ -1,99 +1,109 @@
 module.exports = pushup
 
-var validateProps = require('./lib/validateProps.js')
-  , es = require('event-stream')
-  , knox = require('knox')
-  , Stream = require('stream').Stream
-  , join = require('path').join
+var validateProps = require('./lib/validateProps.js');
+var es = require('event-stream');
+var knox = require('knox');
+var Stream = require('stream').Stream;
+var join = require('path').join;
 
-function pushup (props, callback) {
-  var stream = new Stream()
-    , client = knox.createClient(props)
-    , error = validateProps(props)
-    , ended = false
-    , paused = false
-    , files = null
-    , count = 0
+function pushup(props, options, callback) {
+	if ( typeof options === 'function') {
+		callback = options;
+		options = {};
+	}
+	var stream = new Stream();
+	var client = knox.createClient(props);
+	var error = validateProps(props);
+	var ended = false;
+	var paused = false;
+	var files = null;
+	var count = 0;
 
-  if (error) {
-    if (callback) callback(error)
-    return error
-  }
+	if (error) {
+		callback && callback(error);
+		return error;
+	}
 
-  stream.readable = true
-  stream.writable = true
+	stream.readable = true;
+	stream.writable = true;
 
-  stream.resume = function () {
-    if (ended) {
-      throw new Error('Resume after ended')
-    }
-    paused = false
-    upload()
-    stream.emit('resume')
-  }
+	stream.resume = function() {
+		if (ended) {
+			throw new Error('Resume after ended');
+		}
+		paused = false;
+		upload();
+		stream.emit('resume');
+	};
 
-  stream.pause = function () {
-    if (ended) {
-      throw new Error('Pause after ended')
-    }
-    paused = true
-    stream.emit('pause')
-  }
+	stream.pause = function() {
+		if (ended) {
+			throw new Error('Pause after ended');
+		}
+		paused = true;
+		stream.emit('pause');
+	};
 
-  stream.end = function () {
-    if (count) return
-    ended = true
-    stream.emit('end')
-    if (callback) callback(error)
-  }
+	stream.end = function() {
+		if (count) {
+			return;
+		}
+		ended = true;
+		stream.emit('end');
+		callback && callback(error);
+	};
 
-  stream.write = function (data) {
-    if (!files) {
-      count = 0
-      files = []
-    }
-    
-    files.push(data)
-     
-    upload()
+	stream.write = function(data) {
+		if (!files) {
+			count = 0;
+			files = [];
+		}
 
-    return true 
-  }
+		files.push(data);
 
-  function upload () {
-    if (!files.length && !count) {
-      stream.end()
-      return
-    }
-   
-    stream.pause()
+		upload();
 
-    var file = files.shift()
-    
-    if (!file) {
-      return
-    }
+		return true;
+	}
+	function upload() {
+		if (!files.length && !count) {
+			stream.end();
+			return;
+		}
 
-    var entry = client.putFile(file, '/' + file, responseHandler)
-    
-    count++
+		stream.pause();
 
-    function responseHandler (err, res) {
-      count--
-      if (err) {
-        error = err
-        stream.emit('error', err)
-        if (!ended) stream.resume()
-        return
-      }
-      stream.emit('data', res.socket._httpMessage.url + '\n')
-      if (!ended) stream.resume()
-    }
+		var file = files.shift();
 
-    entry.name = file
-    
-    stream.emit('entry', entry)
-  }
+		if (!file) {
+			return;
+		}
 
-  return stream
+		var entry = client.putFile(file, (options.path || '') + '/' + file, responseHandler);
+
+		count++;
+
+		function responseHandler(err, res) {
+			count--;
+			if (err) {
+				error = err;
+				stream.emit('error', err);
+				if (!ended) {
+					stream.resume();
+				}
+				return;
+			}
+			stream.emit('data', res.socket._httpMessage.url + '\n');
+			if (!ended) {
+				stream.resume();
+			}
+		}
+
+
+		entry.name = file;
+
+		stream.emit('entry', entry);
+	}
+
+	return stream;
 }
